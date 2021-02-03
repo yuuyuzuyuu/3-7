9b73d43698723deabfff12aff31ff0fcd1c6bba9
yuuyuzuyuu <ym.74.mn@gmail.com>
2021-02-03 06:54:01 +0000

    [Start] 3-7

--- CARAVAN/app/controllers/blogs_controller.rb
+++ CARAVAN/app/controllers/blogs_controller.rb
@@ -27,6 +27,9 @@ class BlogsController < ApplicationController
     redirect_to blogs_path(blog)
   end
   
+  def destroy
+  end
+  
   private
   def blog_params
     params.require(:blog).permit(:title, :category, :body)
