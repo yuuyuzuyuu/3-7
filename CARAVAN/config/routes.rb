3a1cc9a94e6d0588a5f0764020aa0393c7ee2cc0
yuuyuzuyuu <ym.74.mn@gmail.com>
2021-02-03 04:14:05 +0000

    [Finish] 3-2

--- CARAVAN/config/routes.rb
+++ CARAVAN/config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'blogs/index'
-  get 'blogs/show'
-  get 'blogs/new'
-  get 'blogs/edit'
+  resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
